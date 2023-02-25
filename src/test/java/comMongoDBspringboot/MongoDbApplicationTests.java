f22653deba5075ffc37e662243b987bd0a0a60a1
Soni Nisha <soni.nishaa24@gmail.com>
2023-02-25 22:34:10 +0530

    first commit

---
+++ src/test/java/comMongoDBspringboot/MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package comMongoDBspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
